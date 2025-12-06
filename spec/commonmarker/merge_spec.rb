12d4e9fff5bbe6a9b29e81c6643b4dd705f8e80a
Peter H. Boling <peter.boling@gmail.com>
2025-12-05 19:45:06 -0700

    🎉 Tada!

---
+++ spec/commonmarker/merge_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Commonmarker::Merge do
+  it "has a version number" do
+    expect(Commonmarker::Merge::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
